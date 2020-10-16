e1f8050cb87c47e101be7c1722e3a270c36eeddc
t-tamaru <t-tamaru@sakura.ad.jp>
2020-10-17 00:37:02 +0900

    ヘッダーの修正

--- header.php
+++ header.php
@@ -14,9 +14,6 @@ get_template_part('header-main');
 
 ?>
 
-<link href="/wp-content/themes/dt-the7-child/css/mypage.css">
-<script src="/wp-content/themes/dt-the7-child/js/mypage.js"></script>
-
 <aside class="fixedMenu -sp">
   <a href="<?php echo home_url(); ?>" class="fixedMenuLink -primary">会員登録</a>
   <a href="<?php echo home_url(); ?>" class="fixedMenuLink -secondary">ログイン</a>
